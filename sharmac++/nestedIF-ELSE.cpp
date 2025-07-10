7a21cb742ca2f4fa7ff156370a4245ad3c029dcd
Naman Sharma <namansharma2005206@gmail.com>
2025-07-10 15:31:55 +0530

    Youngest nested loop

--- sharmac++/nestedIF-ELSE.cpp
+++ sharmac++/nestedIF-ELSE.cpp
@@ -1,31 +1,64 @@
 // NESTED if-else
 // greatest of three using nested loop.
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a,b,c;
+//     cout<<"Enter the first number : ";
+//     cin>>a;
+//     cout<<"Enter the second number : ";
+//     cin>>b;
+//     cout<<"Enter the third number : ";
+//     cin>>c;
+//     if(a>b){  // b can never be greater.
+//         if(a>c){
+//             cout<<a<<"Is greater number";
+//         }
+//         else{
+//             cout<<c<<"Is greater number";
+//         }
+//     }
+//     else{
+//         if(b>c){
+//             cout<<b<<"Is greater number";
+//         }
+//         else{
+//             cout<<c<<"Is greater number";
+//         }
+//     }
+//     return 0;
+// }
+
+//If the age of Ram , Shyam , Ajay are input through the
+//Keybiard. WAP to determinr youngest of the three.
 #include<iostream>
 using namespace std;
 int main(){
-    int a,b,c;
-    cout<<"Enter the first number : ";
-    cin>>a;
-    cout<<"Enter the second number : ";
-    cin>>b;
-    cout<<"Enter the third number : ";
-    cin>>c;
-    if(a>b){  // b can never be greater.
-        if(a>c){
-            cout<<a<<"Is greater number";
-        }
-        else{
-            cout<<c<<"Is greater number";
-        }
+ int  Ram , Shyam , Ajay ;
+ cout<<"Enter the age of Ram : ";
+ cin>>Ram;
+
+  cout<<"Enter the age of Shyam : ";
+  cin>>Shyam;
+  
+   cout<<"Enter the age of Ajay : ";
+   cin>>Ajay;
+  
+ if(Ram<Shyam){
+    if(Ram<Ajay){
+        cout<<Ram<<" Is youngest";
     }
     else{
-        if(b>c){
-            cout<<b<<"Is greater number";
-        }
-        else{
-            cout<<c<<"Is greater number";
-        }
+       cout<<Ajay<<" Is youngest";
     }
-    return 0;
+ }
+ else{
+    if(Shyam<Ajay){
+        cout<<Shyam<<" Is youngest";
+    }
+    else{
+        cout<<Ajay<<"Is youngest";
+    }
+ }
+ return 0;
 }
-
