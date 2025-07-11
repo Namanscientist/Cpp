d4763e12f72dbfc212bda2d2ecaf03506a69d355
Naman Sharma <namansharma2005206@gmail.com>
2025-07-11 21:22:34 +0530

    Quadrant

---
+++ sharmac++/Ternaryoperators.cpp
@@ -0,0 +1,10 @@
+// TERNARY OPERATOR.
+// With ternary operator we pply if-else in same line.
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number : ";
+    cin>>n;
+    (n%2==0) ? cout<<"Even number" : cout<<"Odd number";
+}
