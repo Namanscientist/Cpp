1c7c6ff803bbc7411c98bb81a75c25300fa2e315
Naman Sharma <namansharma2005206@gmail.com>
2025-07-13 22:12:51 +0530

    Ternary operator

--- sharmac++/liesinquadrant.cpp
+++ sharmac++/liesinquadrant.cpp
@@ -2,29 +2,29 @@
 // 1st Quadrant, 2nd Quadrant, 3rd Quadrant, $th Quadrant
 // on the x-axis, y-axis or at origin viz.(0,0);
 
-#include<iostream>
-using namespace std;
-int main(){
-    int x;
-    int y;
-    cout<<"Enter the value of x : ";
-    cin>>x;
-    cout<<"Enter the value of y : ";
-    cin>>y;
-    if(x>0 && y>0){
-        cout<<"x and y both lies in 1st quadrant";
-    }
-    else if(x<0 && y>0){
-        cout<<"x and y both lies in 2nd quadrant";
-    }
-    else if(x<0 && y<0){
-        cout<<"x and y both lies in 3rd quadrant";
-    }
-    else if(x>0 && y<0){
-        cout<<"x and y both lies in 4th quadrant";
-    }
-    else{
-        cout<<"x and y lies on origin";
-    }
-    return 0;
-}
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x;
+//     int y;
+//     cout<<"Enter the value of x : ";
+//     cin>>x;
+//     cout<<"Enter the value of y : ";
+//     cin>>y;
+//     if(x>0 && y>0){
+//         cout<<"x and y both lies in 1st quadrant";
+//     }
+//     else if(x<0 && y>0){
+//         cout<<"x and y both lies in 2nd quadrant";
+//     }
+//     else if(x<0 && y<0){
+//         cout<<"x and y both lies in 3rd quadrant";
+//     }
+//     else if(x>0 && y<0){
+//         cout<<"x and y both lies in 4th quadrant";
+//     }
+//     else{
+//         cout<<"x and y lies on origin";
+//     }
+//     return 0;
+// }
