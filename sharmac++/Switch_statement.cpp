6c8bae40a4ad36e97a64200d830ed3abd5f92367
Naman Sharma <namansharma2005206@gmail.com>
2025-07-13 22:40:48 +0530

    Ques using switch

--- sharmac++/Switch_statement.cpp
+++ sharmac++/Switch_statement.cpp
@@ -4,27 +4,60 @@
 //switch case and functions . The calculator should input
 //two numbers and an operator from user.
 
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int x, y;
+//     char cal;
+//     cout << "Enter the value of x : ";
+//     cin >> x;
+//     cout << "Enter the value of y : ";
+//     cin >> y;
+//     cout << "Enter the operator (+, -, *, /) : ";
+//     cin >> cal;
+
+//     if(cal == '+') cout << x + y;
+//     else if(cal == '-') cout << x - y;
+//     else if(cal == '*') cout << x * y;
+//     else if(cal == '/') {
+//         if (y != 0) cout << x / y;
+//         else cout << "Error: Division by zero!";
+//     }
+//     else cout << "Invalid operator.";
+
+//     return 0;
+// }
+
 #include<iostream>
 using namespace std;
 
-int main(){
-    int x, y;
-    char cal;
-    cout << "Enter the value of x : ";
-    cin >> x;
-    cout << "Enter the value of y : ";
-    cin >> y;
-    cout << "Enter the operator (+, -, *, /) : ";
-    cin >> cal;
+int main() {
+    int choice;
+    cout << "Select a programming language:\n";
+    cout << "1. Python\n";
+    cout << "2. C++\n";
+    cout << "3. Java\n";
+    cout << "4. JavaScript\n";
+    cout << "Enter your choice (1-4): ";
+    cin >> choice;
 
-    if(cal == '+') cout << x + y;
-    else if(cal == '-') cout << x - y;
-    else if(cal == '*') cout << x * y;
-    else if(cal == '/') {
-        if (y != 0) cout << x / y;
-        else cout << "Error: Division by zero!";
+    switch(choice) {
+        case 1:
+            cout << "Python is great for beginners and AI development!";
+            break;
+        case 2:
+            cout << "C++ is powerful for system-level programming.";
+            break;
+        case 3:
+            cout << "Java is widely used in enterprise applications.";
+            break;
+        case 4:
+            cout << "JavaScript brings life to webpages!";
+            break;
+        default:
+            cout << "Invalid choice. Try again!";
     }
-    else cout << "Invalid operator.";
 
     return 0;
 }
