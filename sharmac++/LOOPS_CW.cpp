6fd2d613a76824eab4463d32b6a97c7e5641291a
Naman Sharma <namansharma2005206@gmail.com>
2025-07-15 10:55:21 +0530

    loops

--- sharmac++/LOOPS_CW.cpp
+++ sharmac++/LOOPS_CW.cpp
@@ -50,14 +50,39 @@
 // }
 
 //WAP to print table take input from user.
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x;
+//     cout<<"Enter the value of x : ";
+//     cin>>x;
+//     for(int i=1; i<=10; i++){
+//         cout<<x<<"*"<<i<<"="<<i*x<<endl;
+//     } // x(number) 
+//     return 0;
+// }
+
+//Display this AP- 1,3,5,7,9....n.
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter the value of x : ";// how many no. printed.
+//     cin>>n;
+//     for(int i=1; i<=2*n-1; i+=2){
+//             cout<<i<<" ";
+//     }
+// }
+
 #include<iostream>
 using namespace std;
 int main(){
-    int x;
-    cout<<"Enter the value of x : ";
+    int n,x;
+    cout<<"Enter the number of terms : ";// how many no. printed.
+    cin>>n;
+    cout<<"Enter the no. of gaps : ";
     cin>>x;
-    for(int i=1; i<=10; i++){
-        cout<<x<<"*"<<i<<"="<<i*x<<endl;
-    } // x(number) 
-    return 0;
+    for(int i=1; i<=n*x-1; i+=x){
+            cout<<i<<" ";
+    }
 }
