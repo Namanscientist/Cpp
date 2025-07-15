6b9258ff727f8e6a70aa7253bff134cf6149577e
Naman Sharma <namansharma2005206@gmail.com>
2025-07-15 10:36:55 +0530

    Print Table

--- sharmac++/LOOPS_CW.cpp
+++ sharmac++/LOOPS_CW.cpp
@@ -22,3 +22,42 @@
 //     return 0;
 // }
 
+//Print all even numbers from 1 to 100.
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for(int i=1; i<=100; i++){
+//         if(i%2==0){
+//             cout<<i<<" "<<endl;
+//         }
+//     }
+//     return 0;
+// }
+
+//Print all odd numbers from 1 to 100.
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     // for(int i=1; i<=100; i++){
+//     //     if(i%2!=0){
+//     //         cout<<i<<" "<<endl;
+//     //     }
+//     // }
+//     for(int i=1; i<=100; i=i+2){
+//         cout<<i<<" "<<endl;
+//     }
+//     return 0;
+// }
+
+//WAP to print table take input from user.
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"Enter the value of x : ";
+    cin>>x;
+    for(int i=1; i<=10; i++){
+        cout<<x<<"*"<<i<<"="<<i*x<<endl;
+    } // x(number) 
+    return 0;
+}
